3ae5298b863bc2c0a0f0ddf83114603f54dca073
Gabriel Faria <gabriel.selu@gmail.com>
2023-01-30 21:32:23 -0300

    ..

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ matriz = dados.matriz()
 pen_l = []
 pen_c = []
 
-# calcular necessidade e disponibilidade
-
 
 def necessidade():
     necessidade = []
@@ -60,16 +58,18 @@ def add_necessidade():
 def penalidades_linha():
     m_penal = []
     for a in range(len(matriz)-1):
-        menores_nums = []
-        for value in matriz[a][:-1]:
-            if value >= 0:
-                menores_nums.append(value)
-        if len(menores_nums) > 1:
-            menores_nums = hpq.nsmallest(2, menores_nums)
-            m1 = menores_nums[0]
-            m2 = menores_nums[1]
-            penalidade = m2 - m1
-            m_penal.append(penalidade)
+        linha = matriz[a][:-1]
+        for i in range(len(linha)):
+            if linha[i] < 0:
+                linha[i] = 99999
+        menores_nums = 0
+        menores_nums = hpq.nsmallest(2, linha)
+        m1 = menores_nums[0]
+        m2 = menores_nums[1]
+        penalidade = m2 - m1
+        m_penal.append(penalidade)
+
+    print("PENALIDADE LINHA", m_penal)
     return m_penal
 
 
@@ -77,12 +77,14 @@ def penalidades_coluna():
     result = []  # lista para armazenar as penalidades das colunas
     for i in range(len(matriz[0])-1):
         col = [linha[i] for linha in matriz[:-1]]
-        if any(x < 0 for x in col):
-            result.append(-1)
-        else:
-            two_smallest = hpq.nsmallest(2, col)
-            difference = round(two_smallest[1] - two_smallest[0], 2)
-            result.append(difference)
+        for i in range(len(col)):
+            if col[i] < 0:
+                col[i] = 99999
+        two_smallest = hpq.nsmallest(2, col)
+        difference = round(two_smallest[1] - two_smallest[0], 2)
+        result.append(difference)
+
+    print("PENALIDADE COLUNA", result)
     return result
 
 
@@ -101,11 +103,13 @@ def interacao():
     pen_c = penalidades_coluna()  # VARIAVEL GLOBAL
     maior_v_lin = maior_valor_penalidades_linha(pen_l)
     maior_v_col = maior_valor_penalidades_coluna(pen_c)
+    print("maior valor pen coluna", maior_v_col)
+    print("maior valor pen linha", maior_v_lin)
     index_celula_escolhida_linha = 0
     index_celula_escolhida_coluna = 0
 
-    #FAZER LOGICA PARA RODAR TODAS AS INTERAÇÕES:
-    #USAR WHILE? USAR FOR? FAZER RECURSIVIDADE ATÉ QUE TODAS AS CELULAS SEJAM -1(MENOS DISP E NEC)?
+    # FAZER LOGICA PARA RODAR TODAS AS INTERAÇÕES:
+    # USAR WHILE? USAR FOR? FAZER RECURSIVIDADE ATÉ QUE TODAS AS CELULAS SEJAM -1(MENOS DISP E NEC)?
 
     if (maior_v_col > maior_v_lin):  # se o maior valor está presente na coluna
         index_celula_escolhida_coluna = pen_c.index(
@@ -117,29 +121,71 @@ def interacao():
                 index_celula_escolhida_linha = i
     else:  # se o maior valor está presente na linha
         index_celula_escolhida_linha = pen_l.index(
-           maior_v_lin)  # index do maior valor na linha
+            maior_v_lin)  # index do maior valor na linha
         menor_valor = matriz[index_celula_escolhida_linha][0]
         for i in range(len(matriz[index_celula_escolhida_linha])-1):
             if matriz[index_celula_escolhida_linha][i] < menor_valor:
                 menor_valor = matriz[index_celula_escolhida_linha][i]
                 index_celula_escolhida_coluna = i
-
     necessidade = matriz[-1][index_celula_escolhida_coluna]
     disponibilidade = matriz[index_celula_escolhida_linha][-1]
     valor_atribuido = min(necessidade, disponibilidade)
-    if necessidade < disponibilidade:
+    print("index coluna", index_celula_escolhida_coluna)
+    print("index linha", index_celula_escolhida_linha)
+    # print("necessidade", necessidade)
+    # print("disponibilidade", disponibilidade)
+    # print("valor atribuido", valor_atribuido)
+
+    # if (necessidade == 0):
+    #     for i in range(len(matriz[0])-1):
+    #         matriz[index_celula_escolhida_linha][i] = - \
+    #             1  # linha escolhida vai ser zerada
+    #     matriz[index_celula_escolhida_linha][-1] = matriz[index_celula_escolhida_linha][-1] - disponibilidade
+    #     print("necessidade = 0")
+    # elif (disponibilidade == 0):
+    #     for i in range(len(matriz)-1):
+    #         matriz[i][index_celula_escolhida_coluna] = -1  # coluna escolhida vai ser zerada
+    #     matriz[-1][index_celula_escolhida_coluna] = matriz[-1][index_celula_escolhida_coluna] - necessidade
+    #     print("disponibilidade = 0")
+    if (necessidade == 0) | (disponibilidade == 0):
+        if (disponibilidade == 0):
+            for i in range(len(matriz)-1):
+                matriz[i][index_celula_escolhida_coluna] = - \
+                1  # coluna escolhida vai ser zerada
+            matriz[-1][index_celula_escolhida_coluna] = matriz[-1][index_celula_escolhida_coluna] - necessidade
+            print("disponibilidade = 0")
+        elif (necessidade == 0):
+            for i in range(len(matriz[0])-1):
+                matriz[index_celula_escolhida_linha][i] = - \
+                    1  # linha escolhida vai ser zerada
+            matriz[index_celula_escolhida_linha][-1] = matriz[index_celula_escolhida_linha][-1] - disponibilidade
+            print("necessidade = 0")
+    elif necessidade < disponibilidade:
         for i in range(len(matriz)-1):
-            matriz[i][index_celula_escolhida_coluna] = -1  # coluna escolhida vai ser zerada
+            matriz[i][index_celula_escolhida_coluna] = - \
+                1  # coluna escolhida vai ser zerada
         matriz[-1][index_celula_escolhida_coluna] = matriz[-1][index_celula_escolhida_coluna] - valor_atribuido
+        #print("necessidade < disponibilidade")
     elif disponibilidade < necessidade:
         for i in range(len(matriz[0])-1):
-            matriz[index_celula_escolhida_linha][i] = -1  # linha escolhida vai ser zerada
+            matriz[index_celula_escolhida_linha][i] = - \
+                1  # linha escolhida vai ser zerada
         matriz[index_celula_escolhida_linha][-1] = matriz[index_celula_escolhida_linha][-1] - valor_atribuido
+        #print("disponibilidade < necessidade")
     return matriz
+
 
 def main():
     verifica_dummy()
     add_necessidade()
     add_disponibilidade()
-    print(interacao())
+    x = 0
+    while x <= 31:
+        interacao()
+        print(matriz)
+        x = x + 1
+        print("x", x)
+    ##QUADRO NÃO ESTÁ ZERANDO, TEM QUE ZERAR
+    ##ESTÁ SOBRANDO O DUMMY NA NECESSIDADE
+
 main()
